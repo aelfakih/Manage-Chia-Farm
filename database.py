aec8450ffdb14d0dc0a6d7799f203ee8953c1804
Adonis Elfakih <adonis@aynacorp.com>
2021-07-14 10:37:49 -0400

    fixed issue with SQL statements when migrating to new version

--- database.py
+++ database.py
@@ -6,11 +6,11 @@ def initialize_database() :
     db = sql.connect ( "chia-farm-stats.db" )
     with db :
         db.execute ( "CREATE TABLE if not exists plots (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT, path TEXT,drive TEXT,size FLOAT, type TEXT, valid TEXT); " )
-        #db.execute ( "CREATE TABLE if not exists farm ( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, plot TEXT, path TEXT, drive TEXT, valid TEXT);" )
+        db.execute ( "CREATE UNIQUE INDEX if not exists idx_plots_name ON plots ( name );" )
+
         db.execute ( "CREATE TABLE if not exists plot_directory ( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, path TEXT, drive TEXT, type TEXT, valid TEXT);" )
-        db.execute ( "CREATE UNIQUE INDEX if not exists idx_farm_plot ON farm ( plot );" )
-        #db.execute ( "CREATE UNIQUE INDEX if not exists idx_plots_name ON plots ( name );" )
         db.execute ( "CREATE UNIQUE INDEX if not exists idx_plot_directory_oath ON plot_directory ( path );" )
+
         ## clean up from previous versions
         db.execute ( "DROP INDEX if exists idx_plots_name;" )
         db.execute ( "DROP TABLE if exists farm;" )
