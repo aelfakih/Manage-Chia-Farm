06cd9b83db5d16940facef736f985dc33172ab0d
Adonis Elfakih <adonis@aynacorp.com>
2021-07-15 00:00:27 -0400

    added logic to create db dir

--- database.py
+++ database.py
@@ -3,16 +3,15 @@ from helpers import *
 def initialize_database() :
     import sqlite3 as sql
     """ Define the Database connection """
+    if not os.path.exists ( 'db' ) :
+        os.makedirs ( 'db' )
     db = sql.connect ( "db\chia-farm-stats.db" )
     with db :
-        db.execute ( "CREATE TABLE if not exists plots (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT, path TEXT,drive TEXT,size FLOAT, type TEXT, valid TEXT); " )
-        db.execute ( "CREATE UNIQUE INDEX if not exists idx_plots_name ON plots ( name );" )
+        db.execute ( "CREATE TABLE if not exists plots (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT, path TEXT,drive TEXT,size FLOAT, type TEXT, valid TEXT, unique(name)); " )
+        db.execute ( "CREATE TABLE if not exists plot_directory ( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, path TEXT, drive TEXT, drive_size FLOAT, drive_used FLOAT, drive_free FLOAT, valid TEXT, unique(path));" )
 
-        db.execute ( "CREATE TABLE if not exists plot_directory ( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, path TEXT, drive TEXT, type TEXT, valid TEXT);" )
-        db.execute ( "CREATE UNIQUE INDEX if not exists idx_plot_directory_path ON plot_directory ( path );" )
-
-        ## clean up from previous versions
-        db.execute ( "DROP INDEX if exists idx_plots_name;" )
-        db.execute ( "DROP TABLE if exists farm;" )
+        # Commit your changes in the database
+        db.commit ( )
+    print("* Initialized Database...")
 
 
