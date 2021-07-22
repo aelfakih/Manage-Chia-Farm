0bc43841126bddb89fe924d8acfd3d76bee5caab
Adonis Elfakih <adonis@aynacorp.com>
2021-07-22 11:13:53 -0400

    db cleanup of moved files

--- helpers.py
+++ helpers.py
@@ -756,7 +756,7 @@ def do_scan_farm():
             filename= record[2] + "\\" + record[1]
             if not os.path.exists(filename):
                 logging.info(f"! {filename} not found! removing from plots database ID={id}")
-                do_changes_to_database ( f"DELETE FROM plots SET WHERE id = {id}" )
+                do_changes_to_database ( f"DELETE FROM plots WHERE id = {id}" )
             else:
                 do_changes_to_database(f"UPDATE plots SET scan_ukey = '{session_id}' WHERE id = {id}")
 
